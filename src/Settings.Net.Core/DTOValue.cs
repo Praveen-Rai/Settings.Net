12e2eb2aa97fc64eba0ac9fb61df647ec58c16e1
Praveen Rai <praveen88rai@gmail.com>
2020-03-03 17:51:31 +0530

    Eperiment : Trying to encapsulate the nested Structures by defining a custom class. This approach does not work, since a custom struct/class, is a collection of properties of misc types ( not an array ).

---
+++ src/Settings.Net.Core/DTOValue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Settings.Net.Core
+{
+    public class DTOValue
+    {
+
+        public enum ValueType
+        {
+            String,
+            Number,
+            Boolean,
+            DTOValue,
+            StringArray,
+            NumberArray,
+            BooleanArray,
+            DTOValueArray,
+            Null,
+        }
+
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public ValueType ValueKind { get; set; }
+
+        private ValueType GetValueType()
+        {
+            if (Value == null)
+            {
+                return ValueType.Null;
+            }
+
+            switch (Value.GetType().Name)
+            {
+                case "string":
+                    return ValueType.String;
+
+                case "string[]":
+                    return ValueType.StringArray;
+
+                case "int":
+                    return ValueType.Number;
+
+                case "int[]":
+                    return ValueType.Number;
+
+                default:
+                    return ValueType.Null;
+
+            }
+        }
+    }
+
+
+    /*SettingDTO dto = new SettingDTO() { Name = "CustomNamespace.LineSetting" }
+     * startPointXCoord = new DTOValue { Name="X", ValueKind = ValueType.Number, Value = 0 };
+     * startPointYCoord = new DTOValue { Name="Y", ValueKind = ValueType.Number, Value = 10 };
+     * startPoint = new DTOValue { Name="StartPoint", ValueKind = ValueType.DTOValue, ?? How do we put now ?? Each structure is a collection of Key-Value pairs in itself (collection of properties of misc. Types) }
+     * dto.Value = new DTOValue(){ Name="CustomNamespace.LineSetting", ValueKind = ValueType.DTOValueArray, Value =  }
+     * 
+     */
+}
