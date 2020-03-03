2e6b0271cb81d67472940f31fa2dbaa91ffb85bb
Praveen Rai <praveen88rai@gmail.com>
2020-03-03 20:27:34 +0530

    Experimental : Added another approach with few comments. Use this rather the previous commits.

--- src/Settings.Net.Core/DTOValue.cs
+++ src/Settings.Net.Core/DTOValue.cs
@@ -4,6 +4,12 @@ using System.Text;
 
 namespace Settings.Net.Core
 {
+    // ToDo : Evaluate how the objects will look like in Run-Time in both the approaches.
+    // Evaluate which one if precise in representing the structure.
+    // Evalate which one is easier to iterate. 
+    // Evaluate if there could be any code challenges internally with any of the approaches.
+
+    #region First Approach
     public class DTOValue
     {
 
@@ -17,7 +23,7 @@ namespace Settings.Net.Core
             NumberArray,
             BooleanArray,
             DTOValueArray,
-            Null,
+            Undefined,
         }
 
         public string Name { get; set; }
@@ -30,7 +36,7 @@ namespace Settings.Net.Core
         {
             if (Value == null)
             {
-                return ValueType.Null;
+                return ValueType.Undefined;
             }
 
             switch (Value.GetType().Name)
@@ -48,7 +54,7 @@ namespace Settings.Net.Core
                     return ValueType.Number;
 
                 default:
-                    return ValueType.Null;
+                    return ValueType.Undefined;
 
             }
         }
@@ -62,9 +68,62 @@ namespace Settings.Net.Core
      * StartPoint = new DTOValue { Name="StartPoint", ValueKind = ValueType.DTOValueArray, Value={startPointXCoord, startPointYCoord} 
      * EndPointXCoord = new DTOValue { Name="X", ValueKind = ValueType.Number, Value = 0 };
      * EndPointYCoord = new DTOValue { Name="Y", ValueKind = ValueType.Number, Value = 10 };
-     * EndPoint = new DTOValue { Name="StartPoint", ValueKind = ValueType.DTOValueArray, Value={startPointXCoord, startPointYCoord} 
+     * EndPoint = new DTOValue { Name="EndPoint", ValueKind = ValueType.DTOValueArray, Value={startPointXCoord, startPointYCoord} 
      * dto.Value = new DTOValue(){ Name="CustomNamespace.LineSetting", ValueKind = ValueType.DTOValueArray, Value = {StartPoint, EndPoint} }
      * Note : In case of a Class/Structure, the ValueKind is always going to be DTOValueArray, since a class is generally going to have more than one property.
      *        That raises a question, what for the value type DTOValue is going to be used for ?
+     *        Secondly, for simple types e.g. string, integer, The settingDTO.Value will still be DTOValueObject.
+     *        There is a sort of confusion between a Struct and Struct array. If we can create a new Type for DTOValue of struct/class types, it could be neater. Refer Second Approach
      */
+
+    #endregion
+
+    #region Second Approach
+
+    public enum ValueType2
+    {
+        String,
+        Number,
+        Boolean,
+        DTOStruct,
+        StringArray,
+        NumberArray,
+        BooleanArray,
+        DTOStructArray,
+        Undefined,
+    }
+
+    public class DTOStructProperty
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public ValueType2 ValueKind { get; set; }
+    }
+
+    public class DTOStruct
+    {
+        public List<DTOStructProperty> Properties { get; set; }
+    }
+
+    /*
+    * SettingDTO dto = new SettingDTO() { Name = "CustomNamespace.LineSetting" }
+    * var Line = new DTOStruct();
+    * 
+    * var StartPoint = new DTOStruct();
+    * StartPoint.Properties.Add(new DTOStructPorperty(){Name="X", ValueKind=ValueType2.Number, Value=0});
+    * StartPoint.Properties.Add(new DTOStructPorperty(){Name="Y", ValueKind=ValueType2.Number, Value=10});
+    * Line.Properties.Add(new DTOStructPorperty(){Name="StartPoint", ValueKind=ValueType2.DTOStruct, Value=StartPoint});
+    * 
+    * var EndPoint = new DTOStruct();
+    * EndPoint.Properties.Add(new DTOStructPorperty(){Name="X", ValueKind=ValueType2.Number, Value=0});
+    * EndPoint.Properties.Add(new DTOStructPorperty(){Name="Y", ValueKind=ValueType2.Number, Value=10});
+    * Line.Properties.Add(new DTOStructPorperty(){Name="EndPoint", ValueKind=ValueType2.DTOStruct, Value=StartPoint});
+    * 
+    * dto.ValueType = ValueType2.DTOStruct;
+    * dto.Value = Line;
+    */
+
+    #endregion
 }
